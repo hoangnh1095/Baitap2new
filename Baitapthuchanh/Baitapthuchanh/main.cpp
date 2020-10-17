ee94d338e343c81c966b8f56a0b8982d6e187601
hoangnh1095 <hoangnh1095@gmail.com>
2020-10-17 22:24:26 +0700

    Version 1.0 Phep cong tru nhan chia

---
+++ Baitapthuchanh/Baitapthuchanh/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+int PhepCong(int a, int b)
+{
+	return a + b;
+}
+
+int PhepTru(int a, int b)
+{
+	return a - b;
+}
+
+int PhepNhan(int a, int b)
+{
+	return a * b;
+}
+
+int PhepChia(int a, int b)
+{
+	return a / b;
+}
+
