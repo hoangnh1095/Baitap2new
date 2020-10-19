3f0757f283b41db19e2580ff5b50bcff169d9bc5
thekietak <thekietak123@gmail.com>
2020-10-19 11:17:36 +0700

    Tong 2 so rand

--- Baitapthuchanh/Baitapthuchanh/main.cpp
+++ Baitapthuchanh/Baitapthuchanh/main.cpp
@@ -45,3 +45,41 @@ int HieuRand2so(int a, int b)
 	return tong;
 }
 
+int HieuRand2so(int a, int b)
+{
+	srand(time(NULL));
+	int a = rand() % (100 - 0 + 1);
+	int b = rand() % (100 - 0 + 1);
+	int hieu = a + b;
+	int kq;
+	cout << "Tong cua " << a << " - " << b << "la = ? ";
+	cin >> kq;
+	if (hieu == kq)
+	{
+		cout << " Ket qua nhap vao la DUNG!!";
+	}
+	else
+	{
+		cout << "Ket qua nhap vao la SAI!!";
+	}
+	return tong;
+}
+int TongRand2so(int a, int b)
+{
+	srand(time(NULL));
+	int a = rand() % (100 - 0 + 1);
+	int b = rand() % (100 - 0 + 1);
+	int tong = a + b;
+	int kq;
+	cout << "Tong cua " << a << " + " << b << "la = ? ";
+	cin >> kq;
+	if (tong == kq)
+	{
+		cout << " Ket qua nhap vao la DUNG!!";
+	}
+	else
+	{
+		cout << "Ket qua nhap vao la SAI!!";
+	}
+	return tong;
+}
